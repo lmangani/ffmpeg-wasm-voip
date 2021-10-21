e2910313a41ce8d7285608b75f023dad2fa07eca
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2021-10-21 14:59:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const message = document.getElementById('message');
 const { createFFmpeg } = FFmpeg;
 const ffmpeg = createFFmpeg({
   log: true,
+  corePath: './dist/ffmpeg-core.js',
   progress: ({ ratio }) => {
     message.innerHTML = `Complete: ${(ratio * 100.0).toFixed(2)}%`;
   },
@@ -12,14 +13,14 @@ const transcode = async ({ target: { files }  }) => {
   message.innerHTML = 'Loading ffmpeg-core.js';
   await ffmpeg.load();
   message.innerHTML = 'Start transcoding';
-  await ffmpeg.write(name, files[0]);
+  await ffmpeg.FS('writeFile', name, files[0]);
   var codec = document.getElementById("codec");
   codec.output = codec.value.startsWith('h') ? 'mp4':'mp3';
   var output = "output."+codec.output;
   var cmd = ['-f', codec.value, '-ar 8000', '-i', name, output]
   await ffmpeg.run(cmd.join(' '));
   message.innerHTML = 'Complete transcoding';
-  const data = ffmpeg.read(output);
+  const data = await ffmpeg.FS('readFile', output);
  
   const audio = document.getElementById('output-audio');
   audio.src = URL.createObjectURL(new Blob([data.buffer], { type: 'audio/mpeg' }));

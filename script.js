600efafb47d1b8df817a01f234c32b9e7452242c
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2021-10-21 16:06:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ const transcode = async ({ target: { files }  }) => {
   var cmd = ['', '-i', name, '-f', codec.value, '-ar', 8000, output]
   await ffmpeg.run(cmd.join(' '));
   message.innerHTML = 'Complete transcoding';
-  const data = await ffmpeg.FS('readFile', output);
+  const data = ffmpeg.FS('readFile', output);
   const audio = document.getElementById('output-audio');
   audio.src = URL.createObjectURL(new Blob([data.buffer], { type: 'audio/mpeg' }));
     

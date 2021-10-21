c8c4b0ab6f7750a8735b1680497528759abb1e13
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2021-10-21 15:39:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const ffmpeg = createFFmpeg({
 });
 
 const transcode = async ({ target: { files }  }) => {
+  try {
   const { name } = files[0];
   message.innerHTML = 'Loading ffmpeg-core.js';
   await ffmpeg.load();
@@ -18,12 +19,13 @@ const transcode = async ({ target: { files }  }) => {
   var codec = document.getElementById("codec");
   codec.output = codec.value.startsWith('h') ? 'mp4':'mp3';
   var output = "output."+codec.output;
-  var cmd = ['-f', codec.value, '-ar 8000', '-i', name, output]
+  var cmd = ['-i', name, '-f', codec.value, '-ar 8000', output]
   await ffmpeg.run(cmd.join(' '));
   message.innerHTML = 'Complete transcoding';
   const data = ffmpeg.FS('readFile', output);
- 
   const audio = document.getElementById('output-audio');
   audio.src = URL.createObjectURL(new Blob([data.buffer], { type: 'audio/mpeg' }));
+    
+  } catch(e) {  message.innerHTML = e; }
 }
 document.getElementById('uploader').addEventListener('change', transcode);
